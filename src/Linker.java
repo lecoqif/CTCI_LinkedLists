02d14638e61c60b279bd855e34993f3a4e349436
lecoqif <abhaydais@gmail.com>
2020-02-04 21:28:27 +0000

    Loop Detect

--- src/Linker.java
+++ src/Linker.java
@@ -115,7 +115,15 @@ public class Linker {
     }
 
     public boolean loopdetect(ListNode head){
-        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
     }
 
 }
